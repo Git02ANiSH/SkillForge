1324f65acd16da860c1f61ad3eed8d99b0e3c32d
Git02ANiSH <officialmofijul24x7@gmail.com>
2026-07-04 18:39:33 +0530

    feat: implement branded splash screen

--- app/src/main/java/com/skillforge/app/SplashActivity.java
+++ app/src/main/java/com/skillforge/app/SplashActivity.java
@@ -1,7 +1,8 @@
 package com.skillforge.app;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.os.Handler;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +16,17 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
     }
 }
